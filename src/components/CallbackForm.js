f8202f9f75af67ac913a2c6f24bfba1e32bd6bb2
alero <50052866+a1ero@users.noreply.github.com>
2022-12-03 05:57:20 +0400

    Update CallbackForm.js

--- src/components/CallbackForm.js
+++ src/components/CallbackForm.js
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { Send } from "react-bootstrap-icons";
+import emailjs from '@emailjs/browser'
 
 const CallbackForm = () => {
- 
+    const form = useRef();
+    const [buttonText, setButtonText] = useState('Отправить');
+
+    const sendEmail = (e)=> {
+        e.preventDefault();
+        setButtonText('Отправка...')
+        emailjs.sendForm('service_id', 'template_id', form.current, 'public_key')
+        .then((result) => {
+            console.log(result.text);
+            setButtonText('Успешно')
+        }, 
+        (error) => { 
+            console.log(error.text);
+            setButtonText('Ошибка')
+      });
+    };
+
     return(
         <Container>
             <h5 className="text-center">Для уточнения информации оставьте ваш номер телефона и мы вам перезвоним</h5>
             <br/>
-            <Form method="POST" action="">
+            <Form ref={form} onSubmit={sendEmail}>
                 <Form.Group controlId="formBasicName">
-                    <Form.Control required type="text" name="name" placeholder="Ваше имя"/>
+                    <Form.Control 
+                        required 
+                        type="text" 
+                        name="form_name"  
+                        placeholder="Ваше имя"
+                    />
                 </Form.Group>
                 <br/>
                 <Form.Group controlId="formBasicPhone">
-                    <Form.Control required type="tel" name="phone" placeholder="+79999999999"/>
+                    <Form.Control 
+                        required 
+                        type="tel" 
+                        name="form_phone" 
+                        placeholder="+79999999999"
+                    />
                     <Form.Text>Мы никогда не будем делиться вашим номером с кем-либо еще.</Form.Text>
                 </Form.Group>
                 <br/>
@@ -22,7 +49,7 @@ const CallbackForm = () => {
                     <Form.Check required type="checkbox" label="Согласен на обработку персональных данных"/>
                 </Form.Group>
                 <br/>
-                <Button variant="dark" type="submit"><Send/> Отправить</Button>
+                <Button variant="dark" type="submit"><Send/> {buttonText}</Button>
             </Form>
         </Container>
     );
